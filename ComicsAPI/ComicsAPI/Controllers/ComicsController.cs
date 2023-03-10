4385b8bd7823e221ac482c632dd7c02350ad6860
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-03-10 19:40:00 +0300

    backend v1

---
+++ ComicsAPI/ComicsAPI/Controllers/ComicsController.cs
@@ -0,0 +1,77 @@
+﻿using ComicsAPI.Models;
+using ComicsAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComicsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComicsController : ControllerBase
+    {
+        private readonly IBase<Comic> _comicsRepository;
+        private readonly IBase<ComicPhotos> _comicsPhotosRepository;
+
+        public ComicsController(IBase<Comic> comicsRepository, IBase<ComicPhotos> comicsPhotosRepository)
+        {
+            _comicsRepository = comicsRepository;
+            _comicsPhotosRepository = comicsPhotosRepository;
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<IResult> Get()
+        {
+            var comics = await _comicsRepository.GetAll();
+
+            return Results.Ok(comics);
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public async Task<IResult> Get(int id)
+        {
+            var comic = await _comicsRepository.GetById(id);
+
+            if (comic == null) { return Results.NotFound("Комикс не найден"); }
+
+            return Results.Ok(comic);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IResult> Post(Comic comic)
+        {
+            var result = await _comicsRepository.Add(comic);
+
+            return result ?  Results.Ok("Комикс успешно добавлен") : Results.BadRequest("Произошла ошибка при добавлении комикса");
+        }
+
+        [HttpPost]
+        [Route("addPhotos")]
+        public async Task<IResult> Post(ComicPhotos comicPhotos)
+        {
+            var result = await _comicsPhotosRepository.Add(comicPhotos);
+
+            return result ? Results.Ok("Фото комикса успешно добавлены") : Results.BadRequest("Произошла ошибка при добавлении фото комикса");
+        }
+
+
+        [HttpDelete]
+        [Route("delete")]
+        public async Task<IResult> Delete(int id)
+        {
+            var result = await _comicsRepository.DeleteById(id);
+
+            return result ? Results.Ok("Комикс успешно удален") : Results.BadRequest("Произошла ошибка при удалении комикса");
+        }
+
+        [HttpPut]
+        [Route("update")]
+        public async Task<IResult> Put(Comic comic)
+        {
+            var result = await _comicsRepository.Update(comic);
+
+            return result ? Results.Ok("Комикс успешно обновлен") : Results.BadRequest("Произошла ошибка при обновлении комикса");
+        }
+    }
+}
---
+++ ComicsAPI/ComicsAPI/Repositories/ComicPhotoRepository.cs
@@ -0,0 +1,74 @@
+﻿using ComicsAPI.Data;
+using ComicsAPI.Models;
+using ComicsAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsAPI.Repositories
+{
+    public class ComicPhotoRepository : IBase<ComicPhotos>
+    {
+        private readonly AppDbContext _db;
+
+        public ComicPhotoRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<bool> Add(ComicPhotos item)
+        {
+            try
+            {
+                await _db.ComicsPhotos.AddAsync(item);
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Task<bool> DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ComicPhotos>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ComicPhotos> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void Save()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<bool> Update(ComicPhotos item)
+        {
+            var comicPhotos = await _db.ComicsPhotos.FirstOrDefaultAsync(x => x.Id == item.Id);
+
+            if (comicPhotos == null) { return false; }
+
+            _db.ChangeTracker.Clear();
+
+            try
+            {
+                _db.Entry(comicPhotos).State = EntityState.Modified;
+
+                await _db.SaveChangesAsync();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
